262896ac0751772e2da7af629d344c37e82fd31b
Garrett Jones <gjones@null.net>
2023-07-08 19:55:30 -0600

    added Promise.all and basic wordFrequency function to test with

--- src/backend.ts
+++ src/backend.ts
@@ -5,23 +5,31 @@ const MAX_FILES = 2048;
 class GraphFile {
     public path: vscode.Uri;
 
-    private analysisResultData: string[];
+    private analysisResultData: Map<string, number>;
 
-    constructor(path: vscode.Uri) {
+    constructor(path: vscode.Uri, map: Map<string, number>) {
         this.path = path;
+        this.analysisResultData = map;
     }
 };
 
 export async function analyzeAllFiles(context: vscode.ExtensionContext): Promise<GraphFile[]> {
     const files = await vscode.workspace.findFiles('*', null, MAX_FILES);
+    const filePromises: Promise<GraphFile>[] = [];
     const result: GraphFile[] = [];
 
-    // TODO: Spread these async operations out in 100 instances and use Promise.all to wait for them all at once.
     for (const file of files) {
-        result.push(await analyzeFile(file));
+        filePromises.push(analyzeFile(file));
     }
 
-    return result;
+    try {
+        const results = await Promise.all(filePromises);
+        return results;
+    } catch (error) {
+        // TODO: Handle errors here
+        throw new Error('Error:' +error);
+    }
+
 }
 
 export async function analyzeFile(path: vscode.Uri): Promise<GraphFile> {
@@ -32,6 +40,20 @@ export async function analyzeFile(path: vscode.Uri): Promise<GraphFile> {
 }
 function analyzeText(path: vscode.Uri, text: string): GraphFile {
     // TODO: Derive some useful information from the text here.
-
-    return new GraphFile(path);
+    const frequencyMap = wordFrequency(text);
+    return new GraphFile(path, frequencyMap);
 }
+// calculates the frequency of each word split by non word characters
+function wordFrequency(text: string): Map<string, number> {
+    const words = text.toLowerCase().split(/\W+/);
+    const frequencyMap = new Map<string, number>();
+  
+    for (const word of words) {
+      if (word) {
+        const count = frequencyMap.get(word) || 0;
+        frequencyMap.set(word, count + 1);
+      }
+    }
+  
+    return frequencyMap;
+  }
